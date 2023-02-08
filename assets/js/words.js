10a74c1db2db77f0d871e9a555e5c0ed01748afb
unknown <ryder.george20@gmail.com>
2023-02-08 20:39:22 +0000

    sorted getting button input sorted

--- assets/js/words.js
+++ assets/js/words.js
@@ -1,15 +1,25 @@
-var word = "test"
 
-const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://wordsapiv1.p.rapidapi.com/words/"+ word,
-    "method": "GET",
-    "headers": {
-      "X-RapidAPI-Key": "995fce24e2msh73602efe9782e42p1eeb87jsnee72b111a680",
-      "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
-    }
-  };
-  $.ajax(settings).then(function (response) {
-    console.log(response);
-  });
+// const settings = {
+//     "async": true,
+//     "crossDomain": true,
+//     "url": "https://wordsapiv1.p.rapidapi.com/words/"+ word,
+//     "method": "GET",
+//     "headers": {
+//       "X-RapidAPI-Key": "995fce24e2msh73602efe9782e42p1eeb87jsnee72b111a680",
+//       "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+//     }
+//   };
+//   $.ajax(settings).then(function (response) {
+//     console.log(response);
+//   });
+
+
+function getUserInput(){
+        $("#search-button").on("click", function(event){
+            event.preventDefault();
+            var userInput = $("#search-input").val();
+            console.log(userInput);
+        });
+}
+
+getUserInput();
