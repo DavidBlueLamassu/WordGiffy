88b15f585167ccd269f829566baa3923ddcc5fed
unknown <ryder.george20@gmail.com>
2023-02-08 19:58:19 +0000

    got wordAPI working

---
+++ assets/js/words.js
@@ -0,0 +1,15 @@
+var word = "test"
+
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://wordsapiv1.p.rapidapi.com/words/"+ word,
+    "method": "GET",
+    "headers": {
+      "X-RapidAPI-Key": "995fce24e2msh73602efe9782e42p1eeb87jsnee72b111a680",
+      "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com"
+    }
+  };
+  $.ajax(settings).then(function (response) {
+    console.log(response);
+  });
